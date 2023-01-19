f3f3e76de4db14623c2920f7c316cb1969ea0496
Mendes Sousa <leoprofessionallogytech@outlook.com>
2023-01-19 13:25:12 -0300

    @v1.0.34

--- topicos avançacos de javascript.js
+++ topicos avançacos de javascript.js
@@ -542,3 +542,274 @@ export default function App() {
     </>
   );
 }
+
+console.log('______________________________________________________________________________________________________________________________')
+
+data=[]
+
+
+function Interface(object){
+    
+    data.push(object)
+    
+    return data
+    
+} 
+
+let user=(args)=>{
+    
+    let userData={ ...args }
+    
+    return Interface(userData)
+    
+}
+
+let store=(args)=>{
+    
+    let storeData={ ...args }
+    
+    return Interface(sstoreData)
+    
+}
+
+let product=(args)=>{
+    
+    let prodData={ ...args }
+    
+    return Interface(prodData)
+    
+}
+
+
+//pode ser uma classe ou objeto representando o componente model ou uma função recebendo o arg: req.body.arg nos campos [express.js]
+let client1={
+    id: 1, nome: 'Mendes', email: 'mendes@Gmail.com', endereco: 'Salvador, BA'
+}
+
+//pode ser uma classe ou objeto representando o componente model ou uma função recebendo o arg: req.body.arg nos campos [express.js]
+let store1={
+    id: 33, nome: 'mix supermercado', endereco: 'AV. Aliomar Baleeiro', tipo:'comercio de alimentos'
+}
+
+//pode ser uma classe ou objeto representando o componente model ou uma função recebendo o arg: req.body.arg nos campos [express.js]
+let buy1={
+    id: 145, item:'maratá 200ml', preco:2.35, peso: '10g', emEstoque:'1028 cxs', codigo:012210021, userId: client1, storeId: store1
+}
+
+
+console.log(product(buy1))
+
+console.log('______________________________________________________________________________________________________________________________')
+
+data=[]
+
+
+function Interface(object){
+    
+    data.push(object)
+    
+    return data
+} 
+
+
+let user=({ ...args })=>{
+    
+    return Interface(args)
+}
+
+let store=({ ...args })=>{
+    
+    return Interface(args)
+}
+
+let product=({ ...args })=>{
+    
+    return Interface(args)
+}
+
+
+user({ client1: {id: 1, nome: 'Mendes', email: 'mendes@Gmail.com', endereco: 'Salvador, BA'} })
+
+store({store1: 	{id: 33, nome: 'mix supermercado', endereco: 'AV. Aliomar Baleeiro', tipo:'comercio de alimentos'}})
+
+product({buy1: 	{id: 145, 
+                 item:'maratá 200ml', 
+                 preco:2.35, 
+                 peso: '10g', 
+                 emEstoque:'1028 cxs', 
+                 codigo:012210021, 
+                 userId: JSON.stringify(data[0].client1), 
+                 storeId: JSON.stringify(data[1].store1)
+}})
+
+
+console.log(data[2].buy1)
+
+console.log('______________________________________________________________________________________________________________________________')
+
+
+class Argument {
+    
+    constructor({...args}){
+        this.args=args
+    }
+}
+
+class User extends Argument{
+    
+    constructor({...args}){
+        super(args)
+    }
+}
+
+class Store extends Argument{
+    
+    constructor({...args}){
+        super(args)
+    }
+}
+
+class Product extends Argument{
+    
+    constructor({...args}){
+        super(args)
+    }
+}
+
+
+let user1=new User({client1: {id: 1, nome: 'Mendes', email: 'mendes@Gmail.com', endereco: 'Salvador, BA'}})
+let user2=new User({client2: {id: 2, nome: 'Sousa', email: 'sousa@Gmail.com', endereco: 'Salvador, BA'}})
+
+let store1=new Store({store1: {id: 33, nome: 'mix supermercado', endereco: 'AV. Aliomar Baleeiro', tipo:'comercio de alimentos'}})
+
+let product1=new Product({
+	
+    buy1:{  id: 145, 
+			item:'maratá 200ml', 
+			preco:2.35, 
+			peso: '10g', 
+			emEstoque:'1028 cxs', 
+			codigo:012210021,
+			userId: JSON.stringify(user1.args),
+			storeId: JSON.stringify(store1.args)             
+
+}})
+        
+console.log(product1)
+
+console.log('______________________________________________________________________________________________________________________________')
+
+
+class Argument {
+    
+    constructor({...args}){
+        this.args=args
+    }
+}
+
+class User extends Argument{
+    
+    constructor({...args}){
+        super(args)
+    }
+}
+
+class Store extends Argument{
+    
+    constructor({...args}){
+        super(args)
+    }
+}
+
+class Product extends Argument{
+    
+    constructor({...args}){
+        super(args)
+    }
+}
+
+
+let register=[]
+
+
+let store1=new Store({store1: {id: 33, nome: 'mix supermercado', endereco: 'AV. Aliomar Baleeiro', tipo:'comercio de alimentos'}})
+
+let product1=new Product({
+	
+    buy1:{  id: 145, 
+			item:'maratá 200ml', 
+			preco:2.35, 
+			peso: '10g', 
+			emEstoque:'1028 cxs', 
+			codigo:012210021           
+
+}})
+
+let product2=new Product({
+	
+    buy2:{  id: 155, 
+			item:'Nescau 200ml', 
+			preco:4.85, 
+			peso: '10g', 
+			emEstoque:'322 cxs', 
+			codigo:012254880           
+
+}})
+
+let product3=new Product({
+	
+    buy3:{  id: 47, 
+			item:'sangria olho de boi', 
+			preco:17.56, 
+			peso: '1.3l', 
+			emEstoque:'12 cxs', 
+			codigo:012259999           
+
+}})
+
+let user45=new User({
+    
+    client1: {id: 1, 
+             nome: 'Mendes',
+             email: 'mendes@Gmail.com', 
+             endereco: 'Salvador, BA',
+             buyId: product1.args,
+			 storeId: store1.args
+        
+}})
+
+register.push({user45})
+        
+user45=new User( {
+    client1: {
+        
+             id: 1, 
+             nome: 'Mendes',
+             email: 'mendes@Gmail.com', 
+             endereco: 'Salvador, BA',
+             buyId: product2.args,
+			 storeId: store1.args   
+    
+        
+    }
+})
+
+register.push({user45})
+
+user45=new User( {
+    client1: {
+        
+             id: 1, 
+             nome: 'Mendes',
+             email: 'mendes@Gmail.com', 
+             endereco: 'Salvador, BA',
+             buyId: product3.args,
+			 storeId: store1.args   
+    
+        
+    }
+})
+
+register.push({user45})
+
+console.log(JSON.stringify(register))
